b7367ff32a7578ef03734e95f64cbafd403d2493
Anton Koch <anton.koch@gmail.com>
2017-03-01 04:49:25 +0100

    added feathers apiclient, superagent, raise min node version, run test after build on release

--- src/ui/index.js
+++ src/ui/index.js
@@ -4,7 +4,15 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
 
+import feathers from 'feathers/client';
+import rest from 'feathers-rest/client';
+import superagent from 'superagent';
+
 import AppMain from './components/app-main';
+
+const app = feathers()
+  .configure(rest('http://localhost:3030')
+  .superagent(superagent));
 
 Vue.use(ElementUI);
 
