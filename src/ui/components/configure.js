ca41360ec28394a73b863cb9ff4fbaf9a9e91288
Anton Koch <anton.koch@gmail.com>
2017-04-08 01:08:04 +0200

    updated configure component

--- src/ui/components/configure.js
+++ src/ui/components/configure.js
@@ -11,9 +11,33 @@ class Configure extends Vue {
       }
     };
 
+    /* global window:false */
+    /* jshint node:true */
+    const gui = window.require('nw.gui'),
+      fs = window.require('fs'),
+      configPath = window.require('path').join(gui.App.dataPath, 'config.json');
+    /* jshint node:false */
+
     this.methods = {
       submit() {
-
+        const _self = this;
+        fs.writeFile(configPath, JSON.stringify(_opts.configuration), (err) => {
+          if (err) {
+            return _self.$message({
+              message: err.message,
+              type: 'error'
+            });
+          }
+          _self.$message({
+            message: 'Configuration successfully updated',
+            type: 'success'
+          });
+        });
+      },
+      load() {
+        if (fs.existsSync(configPath)) {
+          _opts.configuration = JSON.parse(fs.readFileSync(configPath));
+        }
       }
     };
 
@@ -22,6 +46,8 @@ class Configure extends Vue {
     };
 
     this.template = '#op-configure-tpl';
+
+    this.load();
   }
 }
 
